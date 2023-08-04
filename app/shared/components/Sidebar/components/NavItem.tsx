dddc42f9f5ab07837ebedd3f10977804466e15ec
Paulo Vitor <pv.marques2010@hotmail.com>
2023-08-04 13:02:52 -0300

    refactor: Mudando a estrutura do componente NavItem

--- app/shared/components/Sidebar/components/NavItem.tsx
+++ app/shared/components/Sidebar/components/NavItem.tsx
@@ -1,13 +1,15 @@
+import React from 'react' // Import React
 import { ChevronDown } from 'lucide-react'
 import { ElementType } from 'react'
 
 interface NavItemProps {
   title: string
   icon: ElementType
+  hasArrow: boolean
 }
 
-export const NavItem = ({ title, icon: Icon }: NavItemProps) => {
-  return (
+export const NavItem = ({ title, icon: Icon, hasArrow }: NavItemProps) => {
+  return hasArrow ? (
     <a
       href="#"
       className="group flex flex-row items-center gap-3 rounded px-3 py-2 hover:bg-violet-50"
@@ -17,6 +19,16 @@ export const NavItem = ({ title, icon: Icon }: NavItemProps) => {
         {title}
       </span>
       <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-violet-300" />
+    </a>
+  ) : (
+    <a
+      href="#"
+      className="group flex flex-row items-center gap-3 rounded px-3 py-2 hover:bg-violet-50"
+    >
+      <Icon className="h-5 w-5 text-zinc-500 group-hover:text-violet-500 " />
+      <span className="font-medium text-zinc-700 group-hover:text-violet-500">
+        {title}
+      </span>
     </a>
   )
 }
