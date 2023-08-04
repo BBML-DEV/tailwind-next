2e3a99aff3c6c4bfcabc3f2c597c7067398cf422
Paulo Vitor <pv.marques2010@hotmail.com>
2023-08-04 11:52:31 -0300

    feat: Criação do Logo e input de busca da aplicação

--- app/shared/components/Sidebar/index.tsx
+++ app/shared/components/Sidebar/index.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { Logo } from './components/Logo'
+import { Search } from 'lucide-react'
 
 export const Sidebar = () => {
-  return <aside className="border-r border-zinc-200 px-5 py-8">lateral</aside>
+  return (
+    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <Search className="h-5 w-5 text-zinc-500" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="
+          flex-1
+          border-0
+          p-0
+          text-zinc-900
+          placeholder-zinc-600
+          outline-none
+          "
+        />
+      </div>
+    </aside>
+  )
 }
