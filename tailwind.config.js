23951f8ab4c69128ad0aad214907ba0c8087e6ce
Paulo Vitor <pv.marques2010@hotmail.com>
2023-08-07 21:58:10 -0300

    feat: Finalização da estilização do input de photo

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         profile: 'max-content 1fr max-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
     },
   },
   plugins: [],
