b31eaa7e9106b8e7daace1541f0ce9b23f005f19
Paulo Vitor <pv.marques2010@hotmail.com>
2023-08-09 10:52:38 -0300

    feat: Criando a feature de adicionar a imagem no componente photo da aplicação

--- app/shared/components/Form/FileInput/Control.tsx
+++ app/shared/components/Form/FileInput/Control.tsx
@@ -1,11 +1,30 @@
 'use client'
 
-import React, { ComponentProps } from 'react'
+import React, { ComponentProps, ChangeEvent } from 'react'
 import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
 export const Control = (props: ControlProps) => {
-  const { id } = useFileInput()
-  return <input type="file" className="sr-only" id={id} {...props} />
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+
+    onFilesSelected(files)
+  }
+
+  return (
+    <input
+      type="file"
+      className="sr-only"
+      onChange={handleFilesSelected}
+      id={id}
+      {...props}
+    />
+  )
 }
--- app/shared/components/Form/FileInput/Root.tsx
+++ app/shared/components/Form/FileInput/Root.tsx
@@ -1,20 +1,29 @@
 'use client'
 
-import React, { ComponentProps, useId, createContext, useContext } from 'react'
+import React, {
+  ComponentProps,
+  useId,
+  createContext,
+  useContext,
+  useState,
+} from 'react'
 
 export type RootProps = ComponentProps<'div'>
 
 interface FileInputContextProps {
   id: string
+  files: File[]
+  onFilesSelected: (files: File[]) => void
 }
 
 const FileInputContext = createContext({} as FileInputContextProps)
 
 export const Root = (props: RootProps) => {
   const id = useId()
+  const [files, setFiles] = useState<File[]>([])
 
   return (
-    <FileInputContext.Provider value={{ id }}>
+    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
       <div {...props} />
     </FileInputContext.Provider>
   )
