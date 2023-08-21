d38c178a04d6aa0af37415399f7a491dde4e795c
Paulo Vitor <pv.marques2010@hotmail.com>
2023-08-21 13:57:28 -0300

    refactor: Mudança de estilo nos botões da bio

--- app/shared/components/Button/index.tsx
+++ app/shared/components/Button/index.tsx
@@ -13,7 +13,8 @@ const button = tv({
       primary: 'bg-violet-600 text-white hover:bg-violet-700',
       outline:
         'border border-zinc-300 dark:hover:bg-zinc-800 text-zinc-700 hover:bg-zinc-50 dark:text-zinc-100 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:bg-zinc-700',
-      ghost: 'rounded-md p-2 hover:bg-zinc-50 dark:text-zinc-400',
+      ghost:
+        'rounded-md p-2 hover:bg-zinc-50 dark:text-zinc-400 dark:hover:bg-zinc-800',
     },
   },
 })
